4bdbcad6287c7c67378c2ece497547f251261885
Kade Dunstone <info@bodyrecode.au>
2026-05-17 16:35:26 +1000

    Revert "Medical clearance: render PDF with PDF.js (react-pdf) so Adobe can't intercept"
    This reverts commit 36b60df15c75b2b2bf00800760dea07e542aa47f.

--- src/app/dashboard/clients/[id]/medical-clearance/page.tsx
+++ src/app/dashboard/clients/[id]/medical-clearance/page.tsx
@@ -3,7 +3,6 @@ import { notFound } from 'next/navigation'
 import Link from 'next/link'
 import ApproveClearanceButton from './approve-clearance-button'
 import RemoveClearanceButton from './remove-clearance-button'
-import PdfViewer from './pdf-viewer'
 
 export default async function MedicalClearancePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
@@ -97,7 +96,11 @@ Kade`
           {isImage ? (
             <img src={docSignedUrl} alt="Medical clearance form" className="w-full rounded-lg border border-stone-700" />
           ) : (
-            <PdfViewer url={docSignedUrl} />
+            <iframe
+              src={docSignedUrl}
+              title="Medical clearance form"
+              className="w-full h-[80vh] rounded-lg border border-stone-700 bg-white"
+            />
           )}
         </div>
       )}
